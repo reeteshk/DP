729504eedef377059e216e166dca306c135bf418
Reetesh Kesarwani <114504936+reeteshk@users.noreply.github.com>
2024-01-25 04:50:17 -0500

    Update printLCS.java

--- DP on Strings/printLCS.java
+++ DP on Strings/printLCS.java
@@ -56,3 +56,54 @@ class Solution{
       lcs(s1,s2);
     }
 }
+
+//Corrected Code fot the printing the LCS 
+
+public class Main {
+    public static void main(String[] args) {
+        lcs("raushan", "reetsh");
+    }
+
+    static void lcs(String s1, String s2) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1))
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                else
+                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+
+        int len = dp[n][m];
+        int i = n;
+        int j = m;
+
+        int index = len - 1;
+        char[] strArray = new char[len]; // Use char array instead of StringBuilder
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                strArray[index] = s1.charAt(i - 1);
+                index--;
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        System.out.println(new String(strArray)); // Convert char array to String for printing
+    }
+}
